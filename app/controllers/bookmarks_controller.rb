d828f0783fed6a65c1a8cb5d7e10c3fe03530e9a
matt shilling <matthewds@sky.com>
2024-07-04 16:12:41 +0100

    still need to fix a few things

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,28 +1,17 @@
 class BookmarksController < ApplicationController
   before_action :set_list
 
+  def new
+    @bookmark = Bookmark.new
+  end
+
   def create
-    @bookmark = @list.bookmarks.new(bookmark_params)
-    if @bookmark.save
-      redirect_to @list, notice: 'Bookmark was successfully created.'
-    else
-      render :new
-    end
+    @bookmark = Bookmark.new(list_params)
   end
 
   def destroy
-    @bookmark = @list.bookmarks.find(params[:id])
+    @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to @list, notice: 'Bookmark was successfully deleted.'
-  end
-
-  private
-
-  def set_list
-    @list = List.find(params[:list_id])
-  end
-
-  def bookmark_params
-    params.require(:bookmark).permit(:url, :title, :movie_id)
+    redirect_to index_path, status: :see_other
   end
 end
